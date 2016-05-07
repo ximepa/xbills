b5cbcb201ba7310b50eb5884d9c9c8bcf70b132e
Michael <ximepa1@yandex.ru>
2016-05-07 20:05:05 +0300

    some changes

--- core/views.py
+++ core/views.py
@@ -6,10 +6,6 @@ from django.core.urlresolvers import reverse
 from .auth_backend import AuthBackend
 from .models import User, Payment, Bill, Fees, Iptv, Tp, ip_to_num, AdminLog, AbonTarifs, AbonUserList, Dv, num_to_ip, UserPi, Street, House, District
 from django.contrib import messages
-#from olltv.api import olltv_auth, olltv_users_list, oll_user_check, oll_user_add, oll_user_bind, oll_user_unbind, oll_user_info, oll_check_bundle, oll_get_device, oll_dev_check, oll_dev_add, oll_disable_bundle, oll_dev_remove, oll_add_bundle, olltv_change_email, olltv_change_userinfo, oll_account_check
-#from olltv.forms import TypeForm, DeviceRemoveForm, DeviceAddForm
-#from olltv.commands import make_conversion
-#from olltv.models import IptvDeviceType, IptvDevice
 from django.conf import settings
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import datetime
@@ -20,9 +16,6 @@ try:
 except:
     olltv_module_path = None
 modules = settings.INSTALLED_APPS
-
-if 'olltv' in modules and os.path.exists(olltv_module_path):
-    from olltv.models import IptvDevice
 
 def custom_redirect(url_name, *args, **kwargs):
     from django.core.urlresolvers import reverse
