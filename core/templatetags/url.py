1ce7c5e5e0c2eaabfb3356cbb2c38a69895624ee
Michael <ximepa1@yandex.ru>
2016-05-09 18:12:41 +0300

    some changes

--- core/templatetags/url.py
+++ core/templatetags/url.py
@@ -17,15 +17,18 @@ def url_replace(request, field, value):
         dict_[field] = value
     return dict_.urlencode()
 
+
 @register.simple_tag
 def url_replace_page(request, field, value):
     dict_ = request.GET.copy()
     dict_[field] = value
     return dict_.urlencode()
 
+
 @register.simple_tag
 def ip_convert(value):
     return num_to_ip(value)
+
 
 @register.simple_tag
 def convert_timestamp_to_time(timestamp):
