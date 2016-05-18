b6bb1b5a19434a89131eb78062d947ce43fe5c04
ximepa <m17tr16x17>
2016-05-18 10:20:20 +0300

    Merge 'origin/master'
    # Conflicts:
    #	core/views.py

--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
 # -*- encoding: utf-8 -*-
+import random
+import socket
 from django.shortcuts import render, HttpResponseRedirect, HttpResponse, render_to_response, RequestContext, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+import pyrad
+import sys
+from pyrad.client import Client
+from pyrad.dictionary import Dictionary
 from .auth_backend import AuthBackend
 from .models import User, Payment, Bill, Fees, Tp, ip_to_num, AdminLog, AbonTarifs, AbonUserList, Dv, num_to_ip, UserPi, Street, House, District, Dv_calls, Nas, ErrorsLog, Dv_log, Admin
 from django.contrib import messages
@@ -62,13 +68,35 @@ def index(request, settings=settings):
 
 
 def nas(request):
+    if 'sessions' in request.GET:
+        sessions = request.GET['sessions']
+        order_by = request.GET.get('order_by', '-user_name')
+        session_list = Dv_calls.objects.filter(nas=request.GET['sessions']).order_by(order_by)
+        all = session_list.count()
+        paginator = Paginator(session_list, 20)
+        page = request.GET.get('page', 1)
+        try:
+            sespage = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            sespage = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            sespage = paginator.page(paginator.num_pages)
+        if int(page) > 5:
+            start = str(int(page)-5)
+        else:
+            start = 1
+        if int(page) < paginator.num_pages-5:
+            end = str(int(page)+5+1)
+        else:
+            end = paginator.num_pages+1
+        page_range = range(int(start), int(end)),
+        for p in page_range:
+            page_list = p
+        pre_end = sespage.paginator.num_pages - 2
     nas_id = Nas.objects.all()
     return render(request, 'nas.html', locals())
-
-
-def static_pools(request):
-
-    return render(request, 'static_pools.html', locals())
 
 
 def client_errors(request, uid):
@@ -242,8 +270,41 @@ def search(request):
         return HttpResponse(dict_resp)
     return render(request, 'search.html', locals())
 
+def SendPacket(srv, req):
+    try:
+        srv.SendPacket(req)
+    except pyrad.client.Timeout:
+        print "RADIUS server does not reply"
+        sys.exit(1)
+    except socket.error, error:
+        print "Network error: " + error[1]
+        sys.exit(1)
+
 
 def client(request, uid):
+    # if 'hangup' in request.GET:
+    #     srv=Client(server="172.16.6.118:3799",
+    #     secret="radsecret",
+    #     dict=Dictionary("dictionary"))
+    #
+    #     req=srv.CreateAcctPacket(User_Name="koka")
+    #
+    #     # req["NAS-IP-Address"]="172.16.6.118"
+    #     # req["NAS-Port"]=0
+    #     # req["NAS-Identifier"]=""
+    #     # req["Called-Station-Id"]="00-04-5F-00-0F-D1"
+    #     # req["Calling-Station-Id"]="00-01-24-80-B3-9C"
+    #     # req["Framed-IP-Address"]="10.0.0.100"
+    #     #
+    #     # print "Sending accounting start packet"
+    #     # req["Acct-Status-Type"]="Start"
+    #     # SendPacket(srv, req)
+    #
+    #     print "Sending accounting stop packet"
+    #     req["Acct-Status-Type"]="Stop"
+    #     req["Acct-Session-Id"]="490331"
+    #     SendPacket(srv, req)
+
     res1 = '<option selected="selected"></option>'
     if 'district' in request.GET:
         district = District.objects.all()
