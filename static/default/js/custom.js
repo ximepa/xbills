7a4ca057c1d46ada9fed0fb57ae98dc58d71fa2b
Michael <ximepa1@yandex.ru>
2016-05-25 11:46:39 +0300

    order by for fees

--- static/default/js/custom.js
+++ static/default/js/custom.js
@@ -2,19 +2,6 @@
  * Created by ximepa on 23.05.16.
  */
 $(document).ready(function(){
-    $(function(){
-        var current = location.pathname;
-        $('.navbar a').each(function(){
-            var $this = $(this);
-            // if the current path is like this link, make it active
-            if($this.attr('href') == current){
-                $this.addClass('linked');
-            }
-        });
-        if ($('.dropdown-menu a').hasClass('linked')){
-            $('#client-menu').addClass('linked');
-        }
-    });
     var body_class = $.cookie('body_class');
     if(body_class) {
     //    console.log('class');
