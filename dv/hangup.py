c6cc1c1593a5d1777258358cfd90d11a1f4e03c7
Michael <ximepa1@yandex.ru>
2016-05-25 16:52:39 +0300

    some changes

--- dv/hangup.py
+++ dv/hangup.py
@@ -5,9 +5,9 @@ from core.models import Nas
 
 
 def Hangup(server, session):
-    ip = Nas.objects.get(id=server).ip
-    sclient = Client(server=ip, secret="radsecret", acctport=3799, dict=Dictionary("dictionary"))
-    req = sclient.CreateAcctPacket(code=pyrad.packet.DisconnectRequest)
+    nas = Nas.objects.get(id=server)
+    client = Client(server=nas.ip, secret="radsecret", acctport=3799, dict=Dictionary("dictionary"))
+    req = client.CreateAcctPacket(code=pyrad.packet.DisconnectRequest)
     req["Acct-Session-Id"] = session
-    reply = sclient.SendPacket(req)
+    reply = client.SendPacket(req)
     return reply
