f702035142c3bd2ae3044a320108d5ae0619d9bb
Michael <ximepa1@yandex.ru>
2016-05-25 17:19:41 +0300

    some changes

---
+++ static/cosmo/js/custom.js
@@ -0,0 +1,31 @@
+/**
+ * Created by ximepa on 23.05.16.
+ */
+$(document).ready(function(){
+    var body_class = $.cookie('body_class');
+    if(body_class) {
+    //    console.log('class');
+        $('body').addClass(body_class);
+    //        var currentHref = $(this).attr('href');
+    //        if(currentHref == currentPage) {
+    //            $(this).attr('class', body_class);
+    //        }
+    //    })
+    }
+    $('#hide-menu').on('click', function() {
+        //$('body').toggleClass('hidden-menu');
+        if ($('body').hasClass('hidden-menu')){
+            $.cookie('body_class', '');
+        } else {
+            $.cookie('body_class', 'hidden-menu');
+        }
+    });
+    $('.minifyme').on('click', function() {
+        //$('body').toggleClass('hidden-menu');
+        if ($('body').hasClass('minified')){
+            $.cookie('body_class', '');
+        } else {
+            $.cookie('body_class', 'minified');
+        }
+    });
+});
